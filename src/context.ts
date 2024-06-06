e0c8088e09334f3e2483ed93cadf285aef24722b
Grant Linville <grant@acorn.io>
2024-06-06 14:31:19 -0400

    Revert "add support for more browsers"
    This reverts commit f098b2b09ae6a5286c0ed3fd6d862cb281aa9d3b.

--- src/context.ts
+++ src/context.ts
@@ -1,41 +1,15 @@
-import { type BrowserContext, chromium, firefox } from '@playwright/test'
+import { type BrowserContext, chromium } from '@playwright/test'
 
-export async function getNewContext (browser: string, sessionDir: string, javaScriptEnabled: boolean): Promise<BrowserContext> {
-  switch (browser) {
-    case 'chrome':
-      return await chromium.launchPersistentContext(
-        sessionDir,
-        {
-          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-          headless: true,
-          viewport: null,
-          channel: 'chrome',
-          args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
-          ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
-          javaScriptEnabled
-        })
-    case 'firefox':
-      return await firefox.launchPersistentContext(
-        sessionDir,
-        {
-          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0 Safari/537.36',
-          headless: true,
-          viewport: null,
-          javaScriptEnabled
-        })
-    case 'edge':
-      return await chromium.launchPersistentContext(
-        sessionDir,
-        {
-          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64',
-          headless: true,
-          viewport: null,
-          channel: 'msedge',
-          args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
-          ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
-          javaScriptEnabled
-        })
-    default:
-      throw new Error(`Unknown browser: ${browser}`)
-  }
+export async function getNewContext (sessionDir: string, javaScriptEnabled: boolean): Promise<BrowserContext> {
+  return await chromium.launchPersistentContext(
+    sessionDir,
+    {
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      headless: true,
+      viewport: null,
+      channel: 'chrome',
+      args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
+      ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
+      javaScriptEnabled
+    })
 }
