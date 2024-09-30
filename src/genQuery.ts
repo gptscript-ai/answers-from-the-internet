2402b031846d3d0299d69e34f5452af688a4cda5
Grant Linville <grant@acorn.io>
2024-09-30 15:00:04 -0400

    fix: add model provider credential
    Signed-off-by: Grant Linville <grant@acorn.io>

--- src/genQuery.ts
+++ src/genQuery.ts
@@ -1,6 +1,7 @@
 import * as gptscript from '@gptscript-ai/gptscript'
 
 export async function genQuery (question: string): Promise<string> {
+  console.error('GPTSCRIPT_URL:', process.env.GPTSCRIPT_URL)
   const tool: gptscript.ToolDef = {
     agents: [],
     arguments: { type: 'object' },
